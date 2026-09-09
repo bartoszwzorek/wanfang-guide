76fc889fa86e17df1ce0acd516a2069078daf9a8
Bartosz Wzorek <wzorek83@gmail.com>
2026-09-09 17:48:35 +0200

    Bump cache for complete CTF materials

--- precache.js
+++ precache.js
@@ -1,4 +1,4 @@
-self.WANFANG_CACHE_VERSION="20260909-approved-great-wall";
+self.WANFANG_CACHE_VERSION="20260909-ctf-notes-1";
 self.WANFANG_CACHE_FILES=[
   "assets/app.js",
   "assets/compendium-core.js",
